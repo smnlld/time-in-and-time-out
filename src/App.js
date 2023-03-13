ff988b1955e9bfaee43218d42874a9745c78f334
Simon Ullado <99321661+smnlld@users.noreply.github.com>
2023-03-13 16:10:52 +0800

    finale

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { Header } from "./components/Header";
 
 function App() {
+  const [timeIn, setTimeIn] = useState(null);
+  const [timeOut, setTimeOut] = useState(null);
+  const [timeDiff, setTimeDiff] = useState(null);
+
+  const getTime = () => {
+    const now = new Date();
+    return now.getTime(); // Return the timestamp in milliseconds
+  };
+
+  const handleTimeIn = () => {
+    setTimeIn(getTime());
+  };
+
+  const handleTimeOut = () => {
+    setTimeOut(getTime());
+  };
+  const handleTimeDiff = () => {
+    if (timeIn && timeOut) {
+      const diffMs = timeOut - timeIn;
+      setTimeDiff(formatTimeFromMs(diffMs));
+      console.log(diffMs);
+    }
+  };
+  const formatTimeFromMs = (ms) => {
+    const hours = Math.floor(ms / 3600000); // 1 hour = 3600000 ms
+    const minutes = Math.floor((ms % 3600000) / 60000); // 1 minute = 60000 ms
+    const seconds = Math.floor((ms % 60000) / 1000); // 1 second = 1000 ms
+
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-screen bg-[#2F4F4F] overflow-auto">
+      <div className="sm:text-center text-white ">
+        <Header />
+      </div>
+      <div className="mx-auto h-auto w-auto flex-center bg-[#708090] relative p-[10px]">
+        <div>
+          <h1 className="sm:text-center text-black text-5xl">
+            Current Time: {new Date().toLocaleTimeString()}
+          </h1>
+        </div>
+        <div className="sm:text-center">
+          <button
+            className="bg-yellow-600 hover:bg-yellow-300 text-white text-xl tracking-widest font-bold py-2 px-4 rounded-lg my-2 mx-4"
+            onClick={handleTimeIn}
+          >
+            Time In
+          </button>
+          <div className="underline text-xl">
+            {timeIn && <p>Time In: {new Date(timeIn).toLocaleString()}</p>}
+          </div>
+        </div>
+        <div className="sm:text-center">
+          <button
+            className="bg-red-600 hover:bg-red-300 text-white tracking-widest font-bold py-2 px-4 rounded-lg my-2 mx-4"
+            onClick={handleTimeOut}
+          >
+            Time Out
+          </button>
+          <div className="underline text-xl">
+            {timeOut && <p>Time Out: {new Date(timeOut).toLocaleString()}</p>}
+          </div>
+        </div>
+        <div className="sm:text-center">
+          <button
+            className="bg-green-600 hover:bg-green-300 text-white tracking-widest font-bold py-2 px-4 rounded-lg my-2 mx-4"
+            onClick={handleTimeDiff}
+          >
+            Time Difference
+          </button>
+          <div className="underline text-xl text-green-500">
+            {timeDiff && (
+              <p>Time Difference: {timeDiff} This is the Time Difference </p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <h1 className="font-black text-5xl tracking-widest font-sans subpixel-antialiased ">
+        TIME IN - TIME OUT{" "}
+      </h1>
+      <p className=" font-medium text-xl tracking-widest font-sans ">
+        BY:{" "}
+        <strong className=" font-extrabold tracking-widest font-sans">
+          Simon Ullado
+        </strong>
+      </p>
+    </>
+  );
+};
